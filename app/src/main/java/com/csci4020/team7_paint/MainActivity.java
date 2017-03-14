5599087d08302f846dcd518721e0d0ac630cc63e
Zachary Pigott <zach.pigott@gmail.com>
2017-03-14 09:00:51 -0500

    Initial commit

---
+++ app/src/main/java/com/csci4020/team7_paint/MainActivity.java
@@ -0,0 +1,13 @@
+package com.csci4020.team7_paint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
