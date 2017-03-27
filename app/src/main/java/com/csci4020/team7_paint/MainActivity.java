e21c19f09502535f13b7132dd2b70223ea06dad6
bri1- <bri1-purple@hotmail.com>
2017-03-27 13:41:44 -0500

    -Updated layouts

--- app/src/main/java/com/csci4020/team7_paint/MainActivity.java
+++ app/src/main/java/com/csci4020/team7_paint/MainActivity.java
@@ -1,10 +1,18 @@
 package com.csci4020.team7_paint;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/csci4020/team7_paint/paint.java
@@ -0,0 +1,24 @@
+package com.csci4020.team7_paint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+/**
+ * Created by brianaschmidt on 3/23/2017.
+ */
+
+public class paint extends View {
+
+    public paint (Context context){
+        super(context);
+    }
+
+    public paint (Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    public paint (Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+
+}
