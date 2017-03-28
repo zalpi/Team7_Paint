74650bf4cfe60eab5522c15d3e7a6653336954a0
bri1- <bri1-purple@hotmail.com>
2017-03-28 08:37:30 -0500

    -Added variables

--- app/src/main/java/com/csci4020/team7_paint/paint.java
+++ app/src/main/java/com/csci4020/team7_paint/paint.java
@@ -1,13 +1,40 @@
 package com.csci4020.team7_paint;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
-/**
- * Created by brianaschmidt on 3/23/2017.
- */
+import android.widget.SeekBar;
 
 public class paint extends View {
+
+
+    //for path
+    private Path draw;
+
+    //for painting
+    private Paint pathPaint, viewPaint, linePaint, rectanglePaint;
+
+    //startup color
+    private int color = 0xFF000000;
+    private int black = Color.parseColor("#000000");
+
+    //canvas
+    private Canvas drawingCanvas;
+
+    //bitmap
+    private Bitmap canvasB;
+
+    //stroke size
+    private float startSize = 4;
+    private float strokeW;
+
+    public enum TOOLS {BRUSH, LINE, RECTANGLE, ERASER, SAVE};
+    private TOOLS currentSelected = TOOLS.BRUSH;
 
     public paint (Context context){
         super(context);
